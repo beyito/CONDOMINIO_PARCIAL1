50bafa473e25ede35c1ac69030698333b613fee6
Sebas <sebasseveriche91@gmail.com>
2025-09-12 20:25:02 -0400

    añadiendo ruta protegida y prototipo de dashboard con cerrar sesion funcional

--- FrontEnd/src/routes/routes.jsx
+++ FrontEnd/src/routes/routes.jsx
@@ -1,12 +1,42 @@
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
 import LoginForm from '../pages/login'
 import Dashboard from '../pages/Dashboard'
-const AppRoutes = () => {
+import ProtectedRoute from '../components/ProtectedRoutes'
+
+function AppRoutes() {
+  const { isAuthenticated, loading } = useAuth()
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center min-h-screen'>
+        <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500'></div>
+      </div>
+    )
+  }
+
   return (
     <Routes>
-      <Route path='/' element={<LoginForm />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      {/* aquí agregas más rutas */}
+      <Route
+        path='/login'
+        element={
+          !isAuthenticated ? <LoginForm /> : <Navigate to='/dashboard' />
+        }
+      />
+
+      <Route
+        path='/dashboard'
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path='/'
+        element={<Navigate to={isAuthenticated ? '/dashboard' : '/login'} />}
+      />
     </Routes>
   )
 }
