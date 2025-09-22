9f8672a7d0e8681289f075bb4429092200b03d10
Benjamin Herbas Miranda <cobubenjamin898@gmail.com>
2025-09-21 21:18:05 -0400

    PREPARANDO MOVIL A PRODUCCION

--- movil_condominio/lib/config/config_db.dart
+++ movil_condominio/lib/config/config_db.dart
@@ -1,9 +1,10 @@
 // lib/config.dart
 class Config {
   // URL del backend, cámbiala aquí según tu entorno
-  static const String baseUrl =
-      'http://192.168.100.12:8000'; // con telefono fisico
+  // static const String baseUrl =
+  //     'http://192.168.100.12:8000'; // con telefono fisico
 
   // static const String baseUrl = 'http://10.0.2.2:8000'; // con emulador flutter
-  // static const String baseUrl = 'https://api.miapp.com'; // para produccion
+  static const String baseUrl =
+      'https://condominio-parcial1.onrender.com'; // para produccion
 }
