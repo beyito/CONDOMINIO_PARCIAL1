546f1a12c85bf243576a269657a884f4442602e1
Benjamin Herbas Miranda <cobubenjamin898@gmail.com>
2025-09-23 18:56:00 -0400

    prueba

--- movil_condominio/lib/services/reservaCopropietario_service.dart
+++ movil_condominio/lib/services/reservaCopropietario_service.dart
@@ -40,31 +40,46 @@ class ReservaCopropietarioService {
   Future<void> adjuntarComprobante(int idReserva, File imagen) async {
     final token = await authService.getToken();
     if (token == null) throw Exception("Usuario no autenticado");
-    final uri = Uri.parse('$baseUrl/adjuntarComprobante/$idReserva/');
-    var request = http.MultipartRequest('PATCH', uri);
+    // final uri = Uri.parse('$baseUrl/adjuntarComprobante/$idReserva/');
+    // var request = http.MultipartRequest('PATCH', uri);
 
-    // Agregar archivo
+    // // Agregar archivo
+    // request.files.add(
+    //   http.MultipartFile(
+    //     'imagen', // nombre del campo que espera tu backend
+    //     imagen.readAsBytes().asStream(),
+    //     imagen.lengthSync(),
+    //     filename: imagen.path.split("/").last,
+    //     contentType: MediaType('image', 'jpeg'), // o png según tu caso
+    //   ),
+    // );
+
+    // // Si necesitas headers de autenticación
+    // request.headers.addAll({
+    //   'Authorization': 'Bearer $token', // o Bearer
+    // });
+    // print("LLEGA HASTA AQUI");
+    // var response = await request.send();
+
+    // if (response.statusCode == 200) {
+    //   print("Imagen subida correctamente");
+    // } else {
+    //   throw Exception("Error al subir comprobante: ${response.statusCode}");
+    // }
+
+    var request = http.MultipartRequest(
+      'PATCH',
+      Uri.parse('$baseUrl/adjuntarComprobante/$idReserva/'),
+    );
+    request.headers['Authorization'] = 'Bearer $token';
     request.files.add(
       http.MultipartFile(
-        'imagen', // nombre del campo que espera tu backend
+        'imagen',
         imagen.readAsBytes().asStream(),
         imagen.lengthSync(),
         filename: imagen.path.split("/").last,
-        contentType: MediaType('image', 'jpeg'), // o png según tu caso
       ),
     );
-
-    // Si necesitas headers de autenticación
-    request.headers.addAll({
-      'Authorization': 'Bearer $token', // o Bearer
-    });
-    print("LLEGA HASTA AQUI");
     var response = await request.send();
-
-    if (response.statusCode == 200) {
-      print("Imagen subida correctamente");
-    } else {
-      throw Exception("Error al subir comprobante: ${response.statusCode}");
-    }
   }
 }
