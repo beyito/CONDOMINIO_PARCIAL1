8cd1c9a4ead10773937dc343d21efb15c3b00984
Benjamin Herbas Miranda <cobubenjamin898@gmail.com>
2025-09-26 23:38:13 -0400

    cambio

--- BACKEND_CONDOMINIO/condominio/settings.py
+++ BACKEND_CONDOMINIO/condominio/settings.py
@@ -52,9 +52,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -116,6 +116,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # localhost
     "http://127.0.0.1:5173",    # movil
     "https://staging.d31m3gtx8emy11.amplifyapp.com",# tu frontend Vite
+    "https://condominio-parcial1.onrender.com",
 ]
 # Permitir cookies / credenciales
 CORS_ALLOW_CREDENTIALS = True
