e72a358df7a3016d23428d0749ed75d3d713ff53
Benjamin Herbas Miranda <cobubenjamin898@gmail.com>
2025-09-28 21:19:22 -0400

    ajustes

--- BACKEND_CONDOMINIO/condominio/utils.py
+++ BACKEND_CONDOMINIO/condominio/utils.py
@@ -1,15 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 import os
-import json
 from dotenv import load_dotenv
 
-load_dotenv()  # Carga variables de .env
+# Obtener la ruta absoluta del archivo de claves
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+firebase_key_path = os.path.join(BASE_DIR, 'secrets', 'firebase_key.json')
 
+# Inicializar Firebase solo una vez
 if not firebase_admin._apps:
-    firebase_key_json = os.environ.get("FIREBASE_KEY_JSON")
-    cred = credentials.Certificate(json.loads(firebase_key_json))
+    cred = credentials.Certificate(firebase_key_path)
     firebase_admin.initialize_app(cred)
+
+
 def enviar_notificacion(token, titulo, cuerpo, data_extra=None):
     message = messaging.Message(
         notification=messaging.Notification(
